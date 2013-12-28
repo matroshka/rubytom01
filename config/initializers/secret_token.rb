16b149252ff190cffd36dd4aef9d7bf2b284d146
matroshka <matroshka.mint@gmail.com>
2013-12-28 18:34:15 +0100

    premier dépot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rubytom01::Application.config.secret_key_base = '6ac216d885ce834924c35af17b3579a58746159ce3b98b316dcc28268ac13ab8c5102740ad1fe45d3b1697761e31cacaf8e37d63e1f1bd2525c9e5b557e20507'
